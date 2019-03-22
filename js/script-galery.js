004d809e82104358f10ef0e5c3ec877f00e4b06a
raniaw <raniaw@web.de>
2019-03-22 17:11:58 +0100

    change

--- js/script-galery.js
+++ js/script-galery.js
@@ -1,11 +1,10 @@
-function myFunction(bilder) {
-
-    var grossBild = document.getElementById("grossesbild"); 
-    var bildtext = document.getElementById("bildtext"); 
-    grossBild.src = bilder.src; 
-    bildtext.innerHTML = bilder.alt; 
-    console.log(bildtext.innerHTML+ " "+ bilder.alt );
-    var shadow = document.getElementsByClassName("hover-shadow cursor");
-    document.getElementsByTagName("img").style=shadow;
-    grossBild.parentElement.style.display = "block"; 
-  }
+function myFunction (bilder) {
+  var grossBild = document.getElementById('grossesbild')
+  var bildtext = document.getElementById('bildtext')
+  grossBild.src = bilder.src
+  bildtext.innerHTML = bilder.alt
+  console.log(bildtext.innerHTML + ' ' + bilder.alt)
+  var shadow = document.getElementsByClassName('hover-shadow cursor')
+  document.getElementsByTagName('img').style = shadow
+  grossBild.parentElement.style.display = 'block'
+}
